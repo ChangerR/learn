42489cbb28acc166f4043d2d218a97e4c4a74c5e
changer <dingjinchengyx@163.com>
2015-12-01 23:20:41 +0800

    fix libsocketio #2

--- libsocketio/HttpClient.cpp
+++ libsocketio/HttpClient.cpp
@@ -145,7 +145,7 @@ public:
             return false;
         CURLcode code = curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, responseCode);
         if (code != CURLE_OK || !(*responseCode >= 200 && *responseCode < 300)) {
-            prinf("Curl curl_easy_getinfo failed: %s", curl_easy_strerror(code));
+            printf("Curl curl_easy_getinfo failed: %s", curl_easy_strerror(code));
             return false;
         }
         // Get some mor data.
