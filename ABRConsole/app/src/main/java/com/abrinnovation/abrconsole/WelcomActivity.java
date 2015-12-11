8dde57956e2ea3111204dc76cbd63e9833b9b7e4
changer <dingjinchengyx@163.com>
2015-12-11 18:35:57 +0800

    add ARBConsole #0

---
+++ ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/WelcomActivity.java
@@ -0,0 +1,13 @@
+package com.abrinnovation.abrconsole;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom);
+    }
+}
