8c8b40a70c31289ca16c013dea84149e3fc9b4a5
changer <dingjinchengyx@163.com>
2015-12-14 22:09:22 +0800

    ABRConsole #3

---
+++ ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/JoystickView.java
@@ -0,0 +1,13 @@
+package com.abrinnovation.abrconsole;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Changer on 2015/12/14.
+ */
+public class JoystickView extends View {
+    public JoystickView(Context context) {
+        super(context);
+    }
+}
--- ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/WelcomActivity.java
+++ ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/WelcomActivity.java
@@ -1,7 +1,11 @@
 package com.abrinnovation.abrconsole;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class WelcomActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class WelcomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcom);
+    }
+
+    public void onConnectClick(View view) {
+        Log.i("ABRConsole","connect");
+        Intent intent = new Intent(this,ShowActivity.class);
+        startActivity(intent);
+    }
+
+    public void onContactUsClick(View view) {
+        Log.i("ABRConsole","contact us");
+        AlertDialog.Builder builder = new AlertDialog.Builder(WelcomActivity.this,R.style.AppCompatAlertDialogStyle);
+
+        builder.setTitle(R.string.contact_us);
+        builder.setMessage(R.string.contact_us_msg);
+        builder.setPositiveButton(R.string.btn_ok,null);
+        builder.show();
+    }
+
+    public void onAboutUsClick(View view) {
+        Log.i("ABRConsole","about us");
+        AlertDialog.Builder builder = new AlertDialog.Builder(WelcomActivity.this,R.style.AppCompatAlertDialogStyle);
+
+        builder.setTitle(R.string.about_us);
+        builder.setMessage(R.string.copyright);
+        builder.setPositiveButton(R.string.btn_ok,null);
+        builder.show();
     }
 }
