516c06ef06eb7c617e9e20672e46c46c732291ca
changer <dingjinchengyx@163.com>
2015-12-16 18:12:09 +0800

    ABRConsole #5

--- ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/JoystickView.java
+++ ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/JoystickView.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -16,41 +19,27 @@ import java.util.jar.Attributes;
  */
 public class JoystickView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private static final int MAX_TOUCHPOINTS = 10;
-    private static final String START_TEXT = "请随便触摸屏幕进行测试";
-    private Paint textPaint = new Paint();
-    private Paint touchPaints[] = new Paint[MAX_TOUCHPOINTS];
-    private int colors[] = new int[MAX_TOUCHPOINTS];
-
     private int width, height;
-    private float scale = 1.0f;
+    private int leid = -1,lex,ley,lexl,leyl;
+    private int reid = -1,rex,rey,rexl,reyl;
+    private Paint paint = null;
+    private Context _context = null;
 
     public JoystickView(Context context,AttributeSet attrs) {
-        super(context,attrs);
+        super(context, attrs);
         SurfaceHolder holder = getHolder();
         holder.addCallback(this);
         setFocusable(true); // 确保我们的View能获得输入焦点
         setFocusableInTouchMode(true); // 确保能接收到触屏事件
         init();
+
+        _context = context;
     }
 
     private void init() {
-        // 初始化10个不同颜色的画笔
-        textPaint.setColor(Color.WHITE);
-        colors[0] = Color.BLUE;
-        colors[1] = Color.RED;
-        colors[2] = Color.GREEN;
-        colors[3] = Color.YELLOW;
-        colors[4] = Color.CYAN;
-        colors[5] = Color.MAGENTA;
-        colors[6] = Color.DKGRAY;
-        colors[7] = Color.WHITE;
-        colors[8] = Color.LTGRAY;
-        colors[9] = Color.GRAY;
-        for (int i = 0; i < MAX_TOUCHPOINTS; i++) {
-            touchPaints[i] = new Paint();
-            touchPaints[i].setColor(colors[i]);
-        }
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        setZOrderOnTop(true);
     }
 
     /**
@@ -58,35 +47,73 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback 
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // 获得屏幕触点数量
-        int pointerCount = event.getPointerCount();
-        if (pointerCount > MAX_TOUCHPOINTS) {
-            pointerCount = MAX_TOUCHPOINTS;
-        }
-
         // 锁定Canvas,开始进行相应的界面处理
         Canvas c = getHolder().lockCanvas();
-        if (c != null) {
-            c.drawColor(Color.BLACK);
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-                // 当手离开屏幕时，清屏
-            } else {
-                // 在每一个触点上绘制一个十字和坐标信息
-                for (int i = 0; i < pointerCount; i++) {
-                    int id = event.getPointerId(i);
-                    int x = (int) event.getX(i);
-                    int y = (int) event.getY(i);
-                    drawCrosshairsAndText(x, y, touchPaints[id], i, id, c);
-                }
+        int pointerCount = event.getPointerCount();
 
-                // 在每一个触点上绘制一个圆
-                for (int i = 0; i < pointerCount; i++) {
+        if (c != null) {
+            //c.drawColor(Color.TRANSPARENT);
+            c.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            if (event.getAction() == MotionEvent.ACTION_UP || event.getActionMasked() == MotionEvent.ACTION_POINTER_UP) {
+
+                Log.i("ABRConsole","Get " + pointerCount + " MotionEvent");
+                for(int i = 0; i < pointerCount;i++) {
+                    int id = event.getPointerId(i);
+
+                    Log.i("ABRConsole","This event id=" + id);
+                    if(id == leid) {
+                        leid = -1;
+                    }
+
+                    if(id == reid) {
+                        reid = -1;
+                    }
+                }
+            } else if(event.getAction() == MotionEvent.ACTION_DOWN || event.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN) {
+
+                for(int i = 0; i < pointerCount; i++) {
                     int id = event.getPointerId(i);
                     int x = (int) event.getX(i);
                     int y = (int) event.getY(i);
-                    drawCircle(x, y, touchPaints[id], c);
+
+                    //Log.i("ABRConsole","This event x=" + x +" width/2=" + width/2 + " id=" + id);
+                    if( x < this.width / 2 && leid == -1 ) {
+                        leid = id;
+                        lexl = lex = x;
+                        leyl = ley = y;
+                    }
+
+                    if( x >= this.width / 2 && reid == -1) {
+                        //Log.i("ABRConsole","set reid = " + id);
+                        reid = id;
+                        rexl = rex = x;
+                        reyl = rey = y;
+
+                    }
+                }
+            } if(event.getAction() == MotionEvent.ACTION_MOVE) {
+                //Log.i("ABRConsole","Get " + pointerCount + " MotionEvent MOVE led=" + leid + " reid=" + reid);
+                if(leid != -1 ||reid != -1) {
+                    for(int i = 0; i < pointerCount;i++) {
+                        int id = event.getPointerId(i);
+                        int x = (int) event.getX(i);
+                        int y = (int) event.getY(i);
+
+                        //Log.i("ABRConsole","This event id=" + id);
+                        if(id == leid) {
+                            lexl = x;
+                            leyl = y;
+                        }
+
+                        if(id == reid) {
+                            rexl = x;
+                            reyl = y;
+                        }
+                    }
                 }
             }
+
+            drawJoystick(c);
 
             // 画完后，unlock
             getHolder().unlockCanvasAndPost(c);
@@ -94,62 +121,76 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback 
         return true;
     }
 
-    /**
-     * 画十字及坐标信息
-     *
-     * @param x
-     * @param y
-     * @param paint
-     * @param ptr
-     * @param id
-     * @param c
-     */
-    private void drawCrosshairsAndText(int x, int y, Paint paint, int ptr,
-                                       int id, Canvas c) {
-        c.drawLine(0, y, width, y, paint);
-        c.drawLine(x, 0, x, height, paint);
-        int textY = (int) ((15 + 20 * ptr) * scale);
-        c.drawText("x" + ptr + "=" + x, 10 * scale, textY, textPaint);
-        c.drawText("y" + ptr + "=" + y, 70 * scale, textY, textPaint);
-        c.drawText("id" + ptr + "=" + id, width - 55 * scale, textY, textPaint);
+    private void drawJoystick(Canvas c) {
+        if(reid != -1)
+            drawJoystickR(rex,rey,rexl,reyl,c);
+        if(leid != -1)
+            drawJoystickL(lex,ley,lexl,leyl,c);
     }
 
-    /**
-     * 画圆
-     *
-     * @param x
-     * @param y
-     * @param paint
-     * @param c
-     */
-    private void drawCircle(int x, int y, Paint paint, Canvas c) {
-        c.drawCircle(x, y, 40 * scale, paint);
+    private void drawJoystickL(int centerx,int centery,int x,int y, Canvas c) {
+
+        int ringWidth = dip2px(_context,6);
+        int innerRadius = dip2px(_context,60);
+        float midRadius = innerRadius + ringWidth / 2;
+        float distance = (float)Math.sqrt(Math.pow(x - centerx, 2) + Math.pow(y - centery,2));
+        float rx = x;
+        float ry = y;
+        paint.setARGB(155,255,0,0);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(ringWidth);
+        c.drawCircle(centerx,centery,innerRadius,paint);
+
+        if(distance > midRadius) {
+            rx = ((rx - centerx) / distance) * midRadius + centerx;
+            ry = ((ry - centery) / distance) * midRadius + centery;
+        }
+
+        paint.setStyle(Paint.Style.FILL);
+        c.drawCircle(rx,ry,dip2px(_context,20),paint);
     }
 
-    /**
-     * 进入程序时背景画成黑色，然后把START_TEXT写到屏幕
-     */
+    private void drawJoystickR(int centerx,int centery,int x,int y, Canvas c) {
+        int lineWidth = dip2px(_context,6);
+        int lineRange = dip2px(_context,60);
+        float radiusY = y;
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(lineWidth);
+        paint.setARGB(155, 255, 0, 0);
+        c.drawLine(centerx, centery - lineRange, centerx, centery + lineRange, paint);
+
+        if(radiusY > centery + lineRange)
+            radiusY = centery + lineRange;
+
+        if(radiusY < centery - lineRange)
+            radiusY = centery - lineRange;
+
+        paint.setStyle(Paint.Style.FILL);
+        c.drawCircle(centerx, radiusY,dip2px(_context,20),paint);
+    }
+
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
         this.width = width;
         this.height = height;
-        if (width > height) {
-            this.scale = width / 480f;
-        } else {
-            this.scale = height / 480f;
-        }
-        textPaint.setTextSize(14 * scale);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
         Canvas c = getHolder().lockCanvas();
         if (c != null) {
-            c.drawColor(Color.BLACK);
-            float tWidth = textPaint.measureText(START_TEXT);
-            c.drawText(START_TEXT, width / 2 - tWidth / 2, height / 2, textPaint);
+            //c.drawColor(0x00000000);
+            c.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
             getHolder().unlockCanvasAndPost(c);
         }
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
+        //super.setZOrderMediaOverlay(true);
     }
 
     public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 }
--- ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/ShowActivity.java
+++ ABRConsole/app/src/main/java/com/abrinnovation/abrconsole/ShowActivity.java
@@ -1,7 +1,10 @@
 package com.abrinnovation.abrconsole;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ShowActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+    }
+
+    public void onAboutUsClick(View view) {
+        Log.i("ABRConsole", "about us");
+        AlertDialog.Builder builder = new AlertDialog.Builder(ShowActivity.this,R.style.AppCompatAlertDialogStyle);
+
+        builder.setTitle(R.string.about_us);
+        builder.setMessage(R.string.copyright);
+        builder.setPositiveButton(R.string.btn_ok, null);
+        builder.show();
     }
 }
